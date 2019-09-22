a7bbdfd6b7a6847f7da3f82a1858801b7b06db5b
Excalibur0w0 <752408303@qq.com>
2019-09-22 21:12:34 +0800

    'i'

--- provider-config/src/main/java/com/inori/cloud/providerconfig/ProviderConfigApplication.java
+++ provider-config/src/main/java/com/inori/cloud/providerconfig/ProviderConfigApplication.java
@@ -3,8 +3,9 @@ package com.inori.cloud.providerconfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ProviderConfigApplication {
